fdd4086ad21deda246ebe16b58d4ba54aa4681ce
Krunal <krunalkadtan19@gmail.com>
2026-03-26 08:02:51 +0000

    User logout feature implemented

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,12 @@
 const userModel = require('./../models/user.model')
 const jwt = require('jsonwebtoken')
 const emailService = require('./../services/email.service')
-
+const tokenBlackListModel = require('./../models/blackList.model')
 /**
 * - user register controller
 * - POST /api/auth/register
 */
-async function userRegisterController (req, res) {
+async function userRegisterController(req, res) {
 
   const { email, password, name } = req.body;
 
@@ -28,16 +28,16 @@ async function userRegisterController (req, res) {
   const token = jwt.sign({
     userID: user._id
   }, process.env.JWT_SECRET, {
-      expiresIn: "3d"
+    expiresIn: "3d"
   })
 
-  res.cookie("token", token)
+  res.clearCookie("token")
 
   res.status(201).json({
     user: {
       _id: user._id,
       email: user.email,
-      name: user.name 
+      name: user.name
     },
     token
   })
@@ -50,7 +50,7 @@ async function userRegisterController (req, res) {
 * - POST /api/auth/login 
 */
 
-async function userLoginController (req, res) {
+async function userLoginController(req, res) {
   const { email, password } = req.body
 
   const user = await userModel.findOne({ email }).select("+password")
@@ -72,7 +72,7 @@ async function userLoginController (req, res) {
   const token = jwt.sign({
     userID: user._id
   }, process.env.JWT_SECRET, {
-      expiresIn: "3d"
+    expiresIn: "3d"
   })
 
   res.cookie("token", token)
@@ -90,7 +90,32 @@ async function userLoginController (req, res) {
   // await emailService.sendLoginEmail(user.email, user.name);
 }
 
-module.exports = { 
+/**
+ * - User Logout controller
+ * - POST /api/auth/logout
+  */
+async function userLogoutController(req, res) {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1]
+
+  if (!token) {
+    return res.status(200).json({
+      message: "User logged out successfully"
+    })
+  }
+
+  res.clearCookie("token")
+
+  await tokenBlackListModel.create({
+    token: token
+  })
+
+  res.status(200).json({
+    message: "User logged out successfully"
+  })
+}
+
+module.exports = {
   userRegisterController,
-  userLoginController
+  userLoginController,
+  userLogoutController
 }
